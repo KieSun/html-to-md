a2eb74d82337fb427ed6503868cb16366a44b614
xuwu <zx597813039@gmail.com>
2020-10-08 22:19:13 +0800

    feat: 完成掘金的转换

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  proseWrap: 'never',
+  tabWidth: 2,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
+import styled from 'styled-components';
+import TurndownService from 'turndown';
+
+const turndownService = new TurndownService({
+  codeBlockStyle: 'fenced',
+  headingStyle: 'atx',
+  bulletListMarker: '-',
+});
+turndownService.addRule('removeCopy', {
+  filter: (node) => {
+    return node.nodeName === 'PRE';
+  },
+  replacement: (content, node, options) => {
+    if (node.children.length === 0) return content;
+    return `${options.fence} ${node.children[0].getAttribute('lang')}
+${content}
+${options.fence}
+    `;
+  },
+});
+
+turndownService.remove((node) => {
+  return node.className === 'copy-code-btn';
+});
+
+const StyledWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const StyledLeft = styled.div`
+  width: 50%;
+  height: 100%;
+  padding: 10px;
+`;
+
+const StyledRight = styled.div`
+  width: 50%;
+  height: 100%;
+  background: aquamarine;
+  padding: 10px;
+`;
 
 function App() {
+  const [text, setText] = useState('');
+  const handleChange = (instance: any) => {
+    setText(turndownService.turndown(instance.getValue()));
+  };
+  console.log(text);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledWrapper>
+      <StyledLeft>
+        <CodeMirror
+          options={{
+            theme: 'md-mirror',
+            keyMap: 'sublime',
+            mode: 'markdown',
+            lineWrapping: true,
+            lineNumbers: false,
+          }}
+          onChange={handleChange}
+        />
+      </StyledLeft>
+      <StyledRight>
+        <CodeMirror
+          value={text}
+          options={{
+            theme: 'md-mirror',
+            keyMap: 'sublime',
+            mode: 'markdown',
+            lineWrapping: true,
+            lineNumbers: false,
+          }}
+        />
+      </StyledRight>
+    </StyledWrapper>
   );
 }
 
